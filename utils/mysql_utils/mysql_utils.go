d59f8e4cba330e8d65d016233b09ae01820865eb
Reed Cowan <reed.cowan@Reeds-MacBook-Pro-2.local>
2022-11-20 18:52:39 -0500

    removed errors utils to import new library

--- utils/mysql_utils/mysql_utils.go
+++ utils/mysql_utils/mysql_utils.go
@@ -1,9 +1,10 @@
 package mysql_utils
 
 import (
-	"bookstore_user-api/utils/errors"
+	"errors"
 	"strings"
 
+	"github.com/cowanrc/bookstore_utils-go/rest_errors"
 	"github.com/go-sql-driver/mysql"
 )
 
@@ -11,18 +12,18 @@ const (
 	ErrorNoRows = "no rows in result set"
 )
 
-func ParseError(err error) *errors.RestErr {
+func ParseError(err error) rest_errors.RestErr {
 	sqlErr, ok := err.(*mysql.MySQLError)
 	if !ok {
 		if strings.Contains(err.Error(), ErrorNoRows) {
-			return errors.NewNotFoundError("no record matching given id")
+			return rest_errors.NewNotFoundError("no record matching given id")
 		}
-		return errors.NewInternalServerError("error parsing database response")
+		return rest_errors.NewInternalServerError("error parsing database response", err)
 	}
 
 	switch sqlErr.Number {
 	case 1062:
-		return errors.NewConflictError("duplicated key")
+		return rest_errors.NewBadRequestError("invalid data")
 	}
-	return errors.NewInternalServerError("error processing request")
+	return rest_errors.NewInternalServerError("error processing request", errors.New("database error"))
 }
